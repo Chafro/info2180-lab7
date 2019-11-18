1c89883904524b2067d72c6890fd1ed2802d3f39
Chad Brown <m.a.r.c.brown@hotmail.com>
2019-11-18 18:04:27 -0500

    query fixed

--- world.php
+++ world.php
@@ -1,22 +1,60 @@
 <?php
 $host = getenv('IP');
-$username = 'lab7_user';
-$password = 'Chadman101';
+$username = 'root';
+$password = '';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$country = $_REQUEST["country"];
+$context = $_REQUEST["context"];
+if ($context == 0) {
+	$coun = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+	$results =$coun->fetchAll(PDO::FETCH_ASSOC);
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	?>
 
 
-$country = $_GET['country'];
-$coun = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
-echo $coun;
+	<table>
+		<tr>
+			<th>Name</th>
+    		<th>Continent</th>
+    		<th>Independence</th>
+    		<th>Head of State</th>
+    	</tr>
+    	<?php foreach ($results as $row): ?>
+    	<tr>
+  			<td><?=$row['name'];?></td>
+  			<td><?=$row['continent'];?></td>
+  			<td><?=$row['independence_year'];?></td>
+  			<td><?=$row['head_of_state'];?></td>
+		</tr>
+		<?php endforeach; ?>
+	</table>
+
+<?php
+	}elseif ($context == 1) {
+		$coun = $conn->query("SELECT cities.name, district, cities.population FROM cities JOIN countries WHERE countries.code = cities.country_code AND countries.name LIKE '%$country%';");
+
+		$results =$coun->fetchAll(PDO::FETCH_ASSOC);
+
+		?>
+
+
+		<table>
+			<tr>
+				<th>Name</th>
+    			<th>District</th>
+    			<th>population</th>
+    		</tr>
+    		<?php foreach ($results as $row): ?>
+    		<tr>
+  				<td><?=$row['name'];?></td>
+  				<td><?=$row['district'];?></td>
+  				<td><?=$row['population'];?></td>
+			</tr>
+			<?php endforeach; ?>
+		</table>
+<?php
+	}
+
 ?>
-
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
